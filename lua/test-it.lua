169fab022c38abbdcfb857ad78fd531b2bf8cd73
hxsggsz <vt.hugo.2021@gmail.com>
2026-03-28 18:25:33 -0300

    feat: add run describe, test and it tests

--- lua/test-it.lua
+++ lua/test-it.lua
@@ -24,6 +24,121 @@ local function open_jest_terminal()
 	return test_buffer_id
 end
 
+local function get_nearest_test_name(valid_func)
+	local node = vim.treesitter.get_node()
+
+	while node do
+		do
+			if node:type() ~= "call_expression" then
+				goto continue
+			end
+
+			local func_name_node = node:child(0)
+			if not func_name_node then
+				goto continue
+			end
+
+			local func_name = vim.treesitter.get_node_text(func_name_node, 0)
+
+			if func_name ~= valid_func then
+				goto continue
+			end
+
+			local arg_list = node:child(1)
+			local first_arg = arg_list and arg_list:child(1)
+
+			if first_arg then
+				local text = vim.treesitter.get_node_text(first_arg, 0)
+				return text:gsub("['\"]", "") -- Remove aspas e retorna
+			end
+		end
+
+		::continue::
+		node = node:parent()
+	end
+
+	return nil
+end
+
+M.run_it_test = function()
+	local current_file = vim.api.nvim_buf_get_name(0)
+	if current_file == "" then
+		return
+	end
+
+	local it_text = get_nearest_test_name("it")
+
+	if not it_text then
+		print("💡 Nenhum 'it' encontrado sob o cursor.")
+		return
+	end
+
+	local buf = open_jest_terminal()
+
+	vim.fn.jobstart({ "npx", "jest", "--colors", current_file, "-t", it_text }, {
+		term = true,
+	})
+
+	vim.api.nvim_set_option_value("number", false, { scope = "local" })
+	vim.cmd("normal! G")
+
+	-- Não se esqueça do atalho para fechar a janela
+	vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = buf, silent = true })
+end
+
+M.run_test_test = function()
+	local current_file = vim.api.nvim_buf_get_name(0)
+	if current_file == "" then
+		return
+	end
+
+	local test_text = get_nearest_test_name("it")
+
+	if not test_text then
+		print("💡 Nenhum 'test' encontrado sob o cursor.")
+		return
+	end
+
+	local buf = open_jest_terminal()
+
+	-- Adicionado o current_file e a flag -t
+	vim.fn.jobstart({ "npx", "jest", "--colors", current_file, "-t", test_text }, {
+		term = true,
+	})
+
+	vim.api.nvim_set_option_value("number", false, { scope = "local" })
+	vim.cmd("normal! G")
+
+	-- Não se esqueça do atalho para fechar a janela
+	vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = buf, silent = true })
+end
+M.run_describe_test = function()
+	local current_file = vim.api.nvim_buf_get_name(0)
+	if current_file == "" then
+		return
+	end
+
+	local describe_text = get_nearest_test_name("describe")
+
+	if not describe_text then
+		print("💡 Nenhum 'describe' encontrado sob o cursor.")
+		return
+	end
+
+	local buf = open_jest_terminal()
+
+	-- Adicionado o current_file e a flag -t
+	vim.fn.jobstart({ "npx", "jest", "--colors", current_file, "-t", describe_text }, {
+		term = true,
+	})
+
+	vim.api.nvim_set_option_value("number", false, { scope = "local" })
+	vim.cmd("normal! G")
+
+	-- Não se esqueça do atalho para fechar a janela
+	vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = buf, silent = true })
+end
+
 M.run_current_file = function()
 	local current_file = vim.api.nvim_buf_get_name(0)
 	if current_file == "" then
@@ -58,6 +173,9 @@ end
 M.setup = function()
 	vim.keymap.set("n", "<leader>ta", M.run_jest, {})
 	vim.keymap.set("n", "<leader>tf", M.run_current_file, {})
+	vim.keymap.set("n", "<leader>td", M.run_describe_test, {})
+	vim.keymap.set("n", "<leader>ti", M.run_it_test, {})
+	vim.keymap.set("n", "<leader>tt", M.run_test_test, {})
 end
 
 return M
