c10cb41037598e0465ccf4fe5ab5652400976b8e
hxsggsz <vt.hugo.2021@gmail.com>
2026-03-28 18:21:02 -0300

    feat: add run current file tests

--- lua/test-it.lua
+++ lua/test-it.lua
@@ -1,13 +1,44 @@
 local M = {}
 
+local test_buffer_id = nil
+
 local function open_jest_terminal()
-	local buf = vim.api.nvim_create_buf(false, true)
+	if test_buffer_id and vim.api.nvim_buf_is_valid(test_buffer_id) then
+		local test_win_id = vim.fn.bufwinid(test_buffer_id)
+		vim.api.nvim_buf_delete(test_buffer_id, { force = true })
 
-	vim.cmd("botright split")
+		if test_win_id ~= -1 and vim.api.nvim_win_is_valid(test_win_id) then
+			vim.api.nvim_set_current_win(test_win_id)
+		else
+			vim.cmd("botright 15split")
+		end
+	else
+		vim.cmd("botright 15split")
+	end
 
-	local win = vim.api.nvim_get_current_win()
-	vim.api.nvim_win_set_buf(win, buf)
-	vim.api.nvim_win_set_height(win, 15)
+	test_buffer_id = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_name(test_buffer_id, "Jest Logs")
+
+	vim.api.nvim_win_set_buf(0, test_buffer_id)
+
+	return test_buffer_id
+end
+
+M.run_current_file = function()
+	local current_file = vim.api.nvim_buf_get_name(0)
+	if current_file == "" then
+		return
+	end
+
+	local buf = open_jest_terminal()
+
+	vim.fn.jobstart({ "npx", "jest", "--colors", current_file }, {
+		term = true,
+	})
+
+	vim.api.nvim_set_option_value("number", false, { scope = "local" })
+	vim.cmd("normal! G")
+	vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = buf, silent = true })
 end
 
 M.run_jest = function()
@@ -21,12 +52,12 @@ M.run_jest = function()
 	vim.opt_local.relativenumber = false
 	vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = buf, silent = true })
 
-	--auto scroll
 	vim.cmd("normal! G")
 end
 
 M.setup = function()
-	vim.keymap.set("n", "<leader>ti", M.run_jest, {})
+	vim.keymap.set("n", "<leader>ta", M.run_jest, {})
+	vim.keymap.set("n", "<leader>tf", M.run_current_file, {})
 end
 
 return M
